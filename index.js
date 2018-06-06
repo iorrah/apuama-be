fc7cc29773d0cb31ee511c8b5da5da3da94f8983
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2018-06-06 22:46:53 +0200

    adding support for /post and /put requests

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const gallery = require('./utils/gallery');
+const bodyParser     = require('body-parser');
+var fs = require("fs");
 
 const app = express();
+
+const DB_PATH = 'db/gallery.json';
+const DB_ENCODE = 'utf8';
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -10,11 +16,51 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Invalid path');
 });
 
 app.get('/api/gallery', (req, res) => {
-  res.send(gallery);
+  fs.readFile(DB_PATH, DB_ENCODE, (err, data) => {
+    res.end(data);
+  });
+});
+
+app.post('/api/gallery/add', (req, res) => {
+  fs.readFile(DB_PATH, DB_ENCODE, (err, data) => {
+    // Arrange
+    let newItem = {};
+    
+    // Act
+    if (req.body) {
+      newItem.id = req.body.id;
+      newItem.img = req.body.img;
+      newItem.title = req.body.titl;
+    }
+
+    // Persist
+    parsedDb = JSON.parse(data);
+    const updatedDb = [ ...parsedDb, newItem ];
+    fs.writeFileSync(DB_PATH, JSON.stringify(updatedDb));
+    res.end(JSON.stringify(updatedDb));
+  });
+});
+
+app.put('/api/gallery/:id/favorite', (req, res) => {
+  fs.readFile(DB_PATH, DB_ENCODE, (err, data) => {
+    // Arrange
+    const id = parseInt(req.params.id, 10);
+    parsedDb = JSON.parse(data);
+
+    // Act
+    const item = parsedDb.find((e) => e.id === id);
+    item.favorited = true;
+
+    // Persist
+    const others = parsedDb.filter((e) => e.id !== id);
+    const updatedDb = [ ...others, item ];
+    fs.writeFileSync(DB_PATH, JSON.stringify(updatedDb));
+    res.end(JSON.stringify(updatedDb));
+  });
 });
 
 const port = process.env.PORT || 3000;
